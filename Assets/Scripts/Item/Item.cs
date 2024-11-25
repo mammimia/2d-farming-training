80dfd507a54274858e8f3c684a15f83a76f4a025
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-11-25 11:07:48 +0300

    ✨ (Item.cs): Create a new Item class with item details and sprite rendering functionality. 📝 (Item.cs.meta): Add metadata for the new Item class file.

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private ItemDetails itemDetail;
+
+    public ItemDetails ItemDetail
+    {
+        get { return itemDetail; }
+        set { itemDetail = value; }
+    }
+
+    private SpriteRenderer theSR;
+
+    private void Awake()
+    {
+        theSR = GetComponentInChildren<SpriteRenderer>();
+        theSR.sprite = itemDetail.itemSprite;
+    }
+
+
+    private void Start()
+    {
+
+    }
+
+}
